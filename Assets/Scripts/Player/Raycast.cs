4d99c8c65af1f09df424bbaa90ffd3b6033e1384
VAuradk <valeri.boschcastella@gmail.com>
2023-04-24 14:28:29 +0200

    eye laser timerCD (not working)

--- Assets/Scripts/Player/Raycast.cs
+++ Assets/Scripts/Player/Raycast.cs
@@ -6,6 +6,10 @@ public class Raycast : MonoBehaviour
     [SerializeField] private int dmgRaycastDone;
     [SerializeField] private float laserRange = 50f;
     [SerializeField] private float laserDuration = 0.05f;
+    [SerializeField] private float timeFire = 0f;
+    [SerializeField] private float fireRate = 2f;
+
+
 
     LineRenderer laserLine; 
 
@@ -13,26 +17,28 @@ public class Raycast : MonoBehaviour
     {
         laserLine = GetComponent<LineRenderer>();   
     }
-    public void Update()
-    {
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitinfo, laserRange))
-        {
-            //Debug.Log(hitinfo.transform.gameObject.name);
-            //Debug.Log("hit something");
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitinfo.distance, Color.red);
+    //public void Update()
+    //{
+    //    if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitinfo, laserRange))
+    //    {
+    //        //Debug.Log(hitinfo.transform.gameObject.name);
+    //        //Debug.Log("hit something");
+    //        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitinfo.distance, Color.red);
 
-        }
-        else
-        {
-            //Debug.Log("hit nothing");
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * laserRange, Color.green);
-        }
-    }
+    //    }
+    //    else
+    //    {
+    //        //Debug.Log("hit nothing");
+    //        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * laserRange, Color.green);
+    //    }
+    //}
     public void RaycastDMG()
     {
+        timeFire += Time.deltaTime;
         laserLine.SetPosition(0, transform.position);
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitinfo, laserRange))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitinfo, laserRange) && timeFire == fireRate)
         {
+            timeFire = 0;
             laserLine.SetPosition(1, hitinfo.point);
             Debug.Log(hitinfo.point);
             hitinfo.collider.gameObject.GetComponent<IHealth>().TakeDamage(dmgRaycastDone, gameObject);
