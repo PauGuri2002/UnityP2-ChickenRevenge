e9ed2a7cb23cd61f9ea640b73f8807a2f32b5524
Valery Bosch <valeri.boschcastella@gmail.com>
2023-04-25 13:55:24 +0200

    egg shoot update!

--- Assets/Scripts/Player/Combat/EggShot.cs
+++ Assets/Scripts/Player/Combat/EggShot.cs
@@ -5,39 +5,59 @@ using UnityEngine.InputSystem;
 
 public class EggShot : AbstractAttack
 {
-    [Header("Egg Shoot")]
     [SerializeField] private GameObject eggPrefab;
-    [SerializeField] private float eggSpeed;
-    [SerializeField] private int dmgEggDone;
-    [SerializeField] private int eggParabolicShoot;
-    [SerializeField] private float eggLiveTime;
-
     [SerializeField] chickenControl chickenControl;
+
+    [Header("Egg Forces")]
+    [SerializeField] private float eggSpeed = 10f;
+    [SerializeField] private float eggParabolicShoot = 15f;
+
+    [Header("Egg DMG")]
+    [SerializeField] private int dmgEggDone = 20;
+
+    [Header("Egg Hidden Secret")]
+    [SerializeField] private float eggMaxCount = 5;
+    [SerializeField] private int eggNumberSpawn = 3;
+    private float eggCount = 0;
+
+    [Header("Egg Destroy")]
+    [SerializeField] private float eggLiveTime = 6f;
+
 
     public override void StartAttack()
     {
         base.StartAttack();
         chickenControl.rotationChicken = 180f;
     }
-
     public override void EndAttack()
     {
         base.EndAttack();
         chickenControl.rotationChicken = 0f;
     }
 
-    public override void PerformAttack()
+    public override void PerformAttack() 
     {
         var egg = Instantiate(eggPrefab, transform.position, transform.rotation);
-        egg.GetComponent<Rigidbody>().velocity = transform.forward*-1 * eggSpeed + transform.up* eggParabolicShoot;
-        //gameObject.GetComponent<IHealth>().TakeDamage(dmgEggDone, gameObject);
-        StartCoroutine(DestoyEgg());
-        //Destroy(egg);
-        Debug.Log("enter");
+
+        if(eggCount <= eggMaxCount)
+        {
+            egg.GetComponent<Rigidbody>().velocity = transform.forward * -1 * eggSpeed + transform.up * eggParabolicShoot;
+            eggCount++;
+        }
+
+        if(eggCount > eggMaxCount) // seguro que hay un código mas limpio para esta condición
+        {
+            for(int i = 1; i < eggNumberSpawn; i++)
+            {
+                var upgradedShoot = Instantiate(eggPrefab, transform.position, transform.rotation);
+                upgradedShoot.GetComponent<Rigidbody>().velocity = -1 * eggSpeed * transform.forward + transform.up * eggParabolicShoot;
+                eggCount = 0;
+            }
+        }
     }
 
-    IEnumerator DestoyEgg()
-    {
-        yield return new WaitForSeconds(eggLiveTime);
-    }
+    //IEnumerator DestoyEgg()
+    //{
+    //    yield return new WaitForSeconds(eggLiveTime);
+    //}
 }
