d8ff5102f8d6b569f88dac66da1ac31c2133bae3
Pau Guri <pauguvi@gmail.com>
2023-04-27 11:55:53 +0200

    added patrolling speed in boss phases

--- Assets/Scripts/KnifeKitchen/KnifeKitchenCombat.cs
+++ Assets/Scripts/KnifeKitchen/KnifeKitchenCombat.cs
@@ -5,17 +5,6 @@ public class KnifeKitchenCombat : MonoBehaviour
     [SerializeField]
     private int hitDamage = 10;
     // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
 
     private void OnTriggerEnter(Collider other)
     {
